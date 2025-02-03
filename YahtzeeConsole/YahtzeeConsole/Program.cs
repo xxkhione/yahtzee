cd67abb711a24287db7d8ba2a509e8a8fb6a0911
Dami <68552016+xxkhione@users.noreply.github.com>
2025-02-03 13:59:21 -0700

    Created the project folder/solution.

---
+++ YahtzeeConsole/YahtzeeConsole/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace YahtzeeConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

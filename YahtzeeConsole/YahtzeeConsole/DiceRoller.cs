1f0a7daca214ac27f0e998f4ac55b081f19c5245
Byeafly <158355453+Byeafly@users.noreply.github.com>
2025-02-04 17:05:14 -0700

    Added diceroller class

---
+++ YahtzeeConsole/YahtzeeConsole/DiceRoller.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YahtzeeConsole
+{
+    public class DiceRoller
+    {
+        private const int min = 1;
+        private const int max = 6;
+        public static List<int> RollDice(int numberOfRolls)
+        { 
+            List<int> diceRolls = new List<int>();
+            for (int i = 0; i < numberOfRolls; i++)
+            {
+                Random random = new Random();
+                diceRolls.Add(random.Next(min, max));
+            }
+            return diceRolls;
+        }
+
+        public static List<int> RollDice(int numberOfRolls, int min, int max)
+        {
+            List<int> diceRolls = new List<int>();
+            for (int i = 0; i < numberOfRolls; i++)
+            {
+                Random random = new Random();
+                diceRolls.Add(random.Next(min, max));
+            }
+            return diceRolls;
+        }
+    }
+}
